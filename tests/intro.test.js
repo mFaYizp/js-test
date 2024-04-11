b8eb07e7340d194798fad0b63ee13b39ae64fed3
mFaYizp <fayizfaz78@gmail.com>
2024-04-11 14:43:32 +0530

    first test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,1 +1,16 @@
-import { test, expect,it, describe } from "vitest";
+import { test, expect, it, describe } from "vitest";
+import { max } from "../src/intro";
+
+describe("max", () => {
+  it("should return the first argument if it's greater ", () => {
+    expect(max(5, 3)).toBe(5);
+  });
+
+  it("should return the second argument if it's greater", () => {
+    expect(max(8, 10)).toBe(10);
+  });
+
+  it("should return the first argument if argument are equal", () => {
+    expect(max(8, 8)).toBe(8);
+  });
+});
