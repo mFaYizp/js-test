66cc24504f1c2d9c762f1b53ea7062928f2b2d44
mFaYizp <fayizfaz78@gmail.com>
2024-04-11 15:54:29 +0530

    last commit

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { test, expect, it, describe } from "vitest";
-import { max } from "../src/intro";
+import { factorial, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it's greater ", () => {
@@ -12,5 +12,49 @@ describe("max", () => {
 
   it("should return the first argument if argument are equal", () => {
     expect(max(8, 8)).toBe(8);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return fizzBuzz if it's divisible of 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return fizz if it's divisible of 3", () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+
+  it("should return Buzz if it's divisible of 5", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+
+  it("should return arg as a string if it's divisible of 5 or 3", () => {
+    expect(fizzBuzz(2)).toBe("2");
+  });
+});
+
+describe("factorial", () => {
+  it("should return 1 if given 0", () => {
+    expect(factorial(0)).toBe(1);
+  });
+
+  it("should return 1 if given 1", () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  it("should return 2 if given 2", () => {
+    expect(factorial(2)).toBe(2);
+  });
+
+  it("should return 6 if given 3", () => {
+    expect(factorial(3)).toBe(6);
+  });
+
+  it("should return 120 if given 5", () => {
+    expect(factorial(5)).toBe(120);
+  });
+
+  it("should return undefined if given negative", () => {
+    expect(factorial(-5)).toBeUndefined;
   });
 });
